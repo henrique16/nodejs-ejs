fa2a9b060c2a2ad01773929040371c808458d9bf
gabriel.henrique <gabriel.holiveira@live.com>
2019-11-22 13:50:34 -0300

    add-.env

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require("express"),
     bodyParser = require("body-parser"),
     expressLayouts = require("express-ejs-layouts"),
@@ -7,7 +8,7 @@ const express = require("express"),
 
 app.set("view engine", "ejs");
 app.use(expressLayouts);
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/views"));
 
 app.get("/", (req, res) => {
